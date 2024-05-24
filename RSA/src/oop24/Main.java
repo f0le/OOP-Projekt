4fab9de6dc06375ef4af956e72bc2f3dd71d9527
f0le <info@folie.dev>
2024-05-24 10:26:45 +0200

    added test

--- RSA/src/oop24/Main.java
+++ RSA/src/oop24/Main.java
@@ -2,15 +2,18 @@ package oop24;
 
 public class Main {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
-        System.out.println("setup test");                                        //[TEST]
+        System.out.println("setup test"); // [TEST]
 
-        Filemanager testFileManager = new Filemanager(                           //[FILEMANAGERTEST]
-            "RSA\\extern\\test.txt"
-        );                                                                       // test.txt manuell erstellen
+        Filemanager testFileManager = new Filemanager( // [FILEMANAGERTEST]
+                "RSA\\extern\\test.txt"); // test.txt manuell erstellen
 
         testFileManager.write("This is test content.");
+        KeyPair keyPair = new KeyPair(431, 19);
+        keyPair.generateKeyPair();
+        System.out.println("Publickey: {" + keyPair.getPublicKey() + "," + keyPair.getGeneratorNumber() + "}");
+        System.out.println("Privatekey : {" + keyPair.getPrivateKey() + "," + keyPair.getGeneratorNumber() + "}");
 
         String content = testFileManager.read();
 

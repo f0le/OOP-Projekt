50e8aba7107f0e8430c7a0a8f385f20566de3a7a
Pterk96 <peter.krahl@icloud.com>
2024-06-09 18:07:20 +0200

    Wimpel Klasse restrukturiert (maybe final)

--- Wimpel/src/oop24/Wimpel.java
+++ Wimpel/src/oop24/Wimpel.java
@@ -2,13 +2,28 @@ package oop24;
 
 public class Wimpel {
 
-    private char wimpelfarbe;
+    private char farbe;
+    private int anzahl;
 
-    public Wimpel() {
+    public Wimpel(char farbe, int anzahl) {
 
+        this.farbe = farbe;
+        this.anzahl = anzahl;
     }
 
-    public char getWimpelfarbe() {
+    public char getFarbe() {
+
+        return this.farbe;
+    }
+
+    public int getAnzahl() {
+
+        return this.anzahl;
+    }
+
+    public void setAnzahl(int anzahl) {
+
+        this.anzahl = anzahl;
     }
 
 }

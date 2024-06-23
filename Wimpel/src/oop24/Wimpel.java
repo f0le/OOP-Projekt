c0db847f7231a962b978517eb4573a7993cb0e3a
f0le <info@folie.dev>
2024-06-23 21:37:33 +0200

    changed Wimpel

--- Wimpel/src/oop24/Wimpel.java
+++ Wimpel/src/oop24/Wimpel.java
@@ -3,12 +3,22 @@ package oop24;
 public class Wimpel {
 
     private char color;
+    private int count;
 
-    public Wimpel(char color) {
+    public Wimpel(char color, int count) {
         this.color = color;
+        this.count = count;
     }
 
     public char getColor() {
         return this.color;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
     }
 }

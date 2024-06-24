f1ac75538daabffd77f4ca86cb76a4c5e0de8718
Pterk96 <peter.krahl@icloud.com>
2024-06-24 16:09:06 +0200

    changes again

--- Wimpel/src/oop24/Wimpelkette.java
+++ Wimpel/src/oop24/Wimpelkette.java
@@ -22,10 +22,13 @@ public class Wimpelkette {
      * @param farben char array
      **/
     public Wimpelkette(int[] anzahl, char[] farben) {
+
         wimpelMap = new HashMap<>();
+
         for (int i = 0; i < anzahl.length; i++) {
             wimpelMap.put(farben[i], anzahl[i]);
         }
+
         besteErgebnisse = new ArrayList<>();
         anzahlBesteLoesungen = 0;
     }
@@ -47,7 +50,7 @@ public class Wimpelkette {
         generiereKetten(new ArrayList<Wimpel>(), wimpelMap);
 
         if (besteErgebnisse.isEmpty()) {
-            
+
             return null;
         } else {
 
@@ -72,13 +75,19 @@ public class Wimpelkette {
      * @return void
      **/
     private void generiereKetten(ArrayList<Wimpel> aktuelleKette, HashMap<Character, Integer> verbleibendeWimpel) {
+        
         if (verbleibendeWimpel.values().stream().allMatch(anzahl -> anzahl == 0)) {
+
             Ergebnis neuesErgebnis = evaluiereKette(aktuelleKette);
+
             if (besteErgebnisse.isEmpty() || neuesErgebnis.istBesser(besteErgebnisse.get(0))) {
+
                 besteErgebnisse.clear();
                 besteErgebnisse.add(neuesErgebnis);
                 anzahlBesteLoesungen = 1;
+
             } else if (neuesErgebnis.istGleich(besteErgebnisse.get(0))) {
+
                 besteErgebnisse.add(neuesErgebnis);
                 anzahlBesteLoesungen++;
             }
@@ -90,7 +99,9 @@ public class Wimpelkette {
         }
 
         for (Character farbe : verbleibendeWimpel.keySet()) {
+
             if (verbleibendeWimpel.get(farbe) > 0) {
+
                 aktuelleKette.add(new Wimpel(farbe));
                 verbleibendeWimpel.put(farbe, verbleibendeWimpel.get(farbe) - 1);
                 generiereKetten(aktuelleKette, verbleibendeWimpel);
@@ -114,19 +125,27 @@ public class Wimpelkette {
      * @return Ergebnis, object which
      **/
     private Ergebnis evaluiereKette(ArrayList<Wimpel> wimpelliste) {
+
         int minDistanz = Integer.MAX_VALUE;
         int minFrequenz = 0;
 
         HashMap<Character, Integer> letztePosition = new HashMap<>();
 
         for (int i = 0; i < wimpelliste.size(); i++) {
+
             char farbe = wimpelliste.get(i).getFarbe();
+            
             if (letztePosition.containsKey(farbe)) {
+
                 int distanz = i - letztePosition.get(farbe);
+
                 if (distanz < minDistanz) {
+
                     minDistanz = distanz;
                     minFrequenz = 1;
+
                 } else if (distanz == minDistanz) {
+
                     minFrequenz++;
                 }
             }

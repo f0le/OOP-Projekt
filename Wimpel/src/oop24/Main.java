fa05f53958ea63f2e30740cd35abb45e0fa45744
f0le <info@folie.dev>
2024-06-24 11:50:32 +0200

    changes

--- Wimpel/src/oop24/Main.java
+++ Wimpel/src/oop24/Main.java
@@ -1,33 +1,134 @@
 package oop24;
 
 public class Main {
+    /*
+     * public static void main(String[] args) {
+     * 
+     * char[] wimpelfarbe = new char[] { 'r', 'b', 'g', 'w', 's' };
+     * int[] wimpelanzahl = new int[] { 4, 3, 2, 4, 2 };
+     * 
+     * char[] wimpelfarbe1 = new char[] { 'r', 'b', 'g' };
+     * int[] wimpelanzahl1 = new int[] { 4, 3, 2 };
+     * 
+     * char[] wimpelfarbe2 = new char[] { 'r', 'b' };
+     * int[] wimpelanzahl2 = new int[] { 2, 1 };
+     * 
+     * char[] wimpelfarbe3 = new char[] { 'r', 'b', 'g', 's' };
+     * int[] wimpelanzahl3 = new int[] { 4, 3, 2, 2 };
+     * 
+     * Wimpelkette wimpelkettezwei = new Wimpelkette(wimpelanzahl3, wimpelfarbe3);
+     * Ergebnis ergebnis = wimpelkettezwei.generiereOptimaleKette();
+     * 
+     * System.out.println("Beste Kette: " + ergebnis.getKette());
+     * System.out.println("Qualitaet: (" + ergebnis.getMinDistanz() + "," +
+     * ergebnis.getMinFrequenz() + ")");
+     * 
+     * // Wimpelkette wimpelkette1 = new Wimpelkette(wimpelanzahl1, wimpelfarbe1);
+     * // wimpelkette1.printWimpelkette();
+     * // wimpelkette1.printQuality();
+     * 
+     * // Wimpelkette wimpelkette2 = new Wimpelkette(wimpelanzahl2, wimpelfarbe2);
+     * // wimpelkette2.printWimpelkette();
+     * // wimpelkette2.printQuality();
+     * }
+     */
+
+    public enum WimpelFarbe {
+        ROT, BLAU, WEISS, SCHWARZ, GELB;
+    }
 
     public static void main(String[] args) {
+        // test1();
+        // test2();
+        // test3();
+        run3();
+        run2();
+        run1();
+    }
 
-        char[] wimpelfarbe = new char[] { 'r', 'b', 'g', 'w', 's' };
-        int[] wimpelanzahl = new int[] { 4, 3, 2, 4, 2 };
+    private static void run3() {
+        HashMap<WimpelFarbe, Integer> verfuegbareWimpel = new HashMap<>();
+        verfuegbareWimpel.put(WimpelFarbe.ROT, 3);
+        verfuegbareWimpel.put(WimpelFarbe.BLAU, 3);
+        verfuegbareWimpel.put(WimpelFarbe.WEISS, 3);
+        Wimpelkette2 besteKette = bestmoeglicheKette(verfuegbareWimpel);
+        besteKette.aufhaengen();
+        System.out.println(
+                "Qualität(" + besteKette.qualitaet.minimalAbstand + ", " + besteKette.qualitaet.haeufigkeit + ")");
+    }
 
-        char[] wimpelfarbe1 = new char[] { 'r', 'b', 'g' };
-        int[] wimpelanzahl1 = new int[] { 4, 3, 2 };
+    private static void run2() {
+        HashMap<WimpelFarbe, Integer> verfuegbareWimpel = new HashMap<>();
+        verfuegbareWimpel.put(WimpelFarbe.ROT, 1);
+        verfuegbareWimpel.put(WimpelFarbe.BLAU, 2);
+        verfuegbareWimpel.put(WimpelFarbe.WEISS, 3);
+        verfuegbareWimpel.put(WimpelFarbe.SCHWARZ, 4);
+        Wimpelkette2 besteKette = bestmoeglicheKette(verfuegbareWimpel);
+        besteKette.aufhaengen();
+        System.out.println(
+                "Qualität(" + besteKette.qualitaet.minimalAbstand + ", " + besteKette.qualitaet.haeufigkeit + ")");
+    }
 
-        char[] wimpelfarbe2 = new char[] { 'r', 'b' };
-        int[] wimpelanzahl2 = new int[] { 2, 1 };
+    private static void run1() {
+        HashMap<WimpelFarbe, Integer> verfuegbareWimpel = new HashMap<>();
+        verfuegbareWimpel.put(WimpelFarbe.ROT, 4);
+        verfuegbareWimpel.put(WimpelFarbe.BLAU, 3);
+        verfuegbareWimpel.put(WimpelFarbe.WEISS, 4);
+        verfuegbareWimpel.put(WimpelFarbe.SCHWARZ, 2);
+        verfuegbareWimpel.put(WimpelFarbe.GELB, 2);
+        Wimpelkette2 besteKette = bestmoeglicheKette(verfuegbareWimpel);
+        besteKette.aufhaengen();
+        System.out.println(
+                "Qualität(" + besteKette.qualitaet.minimalAbstand + ", " + besteKette.qualitaet.haeufigkeit + ")");
+    }
 
-        char[] wimpelfarbe3 = new char[] { 'r', 'b', 'g', 's' };
-        int[] wimpelanzahl3 = new int[] { 4, 3, 2, 2 };
+    private static void test1() {
+        List<WimpelFarbe> testWimpelListe = new ArrayList<>();
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        Wimpelkette2 testKette = new Wimpelkette2(testWimpelListe);
+        testKette.berechneQualitaet();
+        System.out.print("Qualität(0.0,3)==");
+        System.out.println(
+                "Qualität(" + testKette.qualitaet.minimalAbstand + ", " + testKette.qualitaet.haeufigkeit + ")");
+    }
 
-        Wimpelkette wimpelkettezwei = new Wimpelkette(wimpelanzahl3, wimpelfarbe3);
-        Ergebnis ergebnis = wimpelkettezwei.generiereOptimaleKette();
+    private static void test2() {
+        List<WimpelFarbe> testWimpelListe = new ArrayList<>();
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        Wimpelkette2 testKette = new Wimpelkette2(testWimpelListe);
+        testKette.berechneQualitaet();
+        System.out.print("Qualität(0.0,5)==");
+        System.out.println(
+                "Qualität(" + testKette.qualitaet.minimalAbstand + ", " + testKette.qualitaet.haeufigkeit + ")");
+    }
 
-        System.out.println("Beste Kette: " + ergebnis.getKette());
-        System.out.println("Qualitaet: (" + ergebnis.getMinDistanz() + "," + ergebnis.getMinFrequenz() + ")");
-
-        // Wimpelkette wimpelkette1 = new Wimpelkette(wimpelanzahl1, wimpelfarbe1);
-        // wimpelkette1.printWimpelkette();
-        // wimpelkette1.printQuality();
-
-        // Wimpelkette wimpelkette2 = new Wimpelkette(wimpelanzahl2, wimpelfarbe2);
-        // wimpelkette2.printWimpelkette();
-        // wimpelkette2.printQuality();
+    private static void test3() {
+        List<WimpelFarbe> testWimpelListe = new ArrayList<>();
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.WEISS);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.WEISS);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        Wimpelkette2 testKette = new Wimpelkette2(testWimpelListe);
+        testKette.berechneQualitaet();
+        System.out.print("Qualität(0.0,9)==");
+        System.out.println(
+                "Qualität(" + testKette.qualitaet.minimalAbstand + ", " + testKette.qualitaet.haeufigkeit + ")");
     }
 }
--- Wimpel/src/oop24/Wimpelkette.java
+++ Wimpel/src/oop24/Wimpelkette.java
@@ -1,86 +1,237 @@
-package oop24;
-
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
-public class Wimpelkette {
+//finale version
+public class Wimpelkette2 {
+    private Qualitaet qualitaet;
+    private List<WimpelFarbe> wimpelListe = new ArrayList<>();
+    private HashMap<WimpelFarbe, Integer> wimpelFarbeListe = new HashMap<>();
 
-    private ArrayList<Wimpel> wimpelliste;
+    public class Qualitaet {
+        public double minimalAbstand;
+        public int haeufigkeit;
 
-    public Wimpelkette(int[] anzahl, char[] farben) {
-        wimpelliste = new ArrayList<>();
-        for (int i = 0; i < anzahl.length; i++) {
-            for (int j = 0; j < anzahl[i]; j++) {
-                wimpelliste.add(new Wimpel(farben[i]));
-            }
-        }
-    }
-
-
-    public Ergebnis generiereOptimaleKette() {
-        ArrayList<Ergebnis> ergebnisse = new ArrayList<>();
-        generierePermutationen(wimpelliste, 0, ergebnisse);
-
-        if (ergebnisse.isEmpty()) {
-            return null;
-        } else {
-            System.out.println(ergebnisse.size());
-            return ergebnisse.get(0);
-        }
-    }
-
-    private void generierePermutationen(ArrayList<Wimpel> wimpelliste, int index, ArrayList<Ergebnis> ergebnisse) {
-        if (index == wimpelliste.size()) {
-            Ergebnis neuesErgebnis = evaluiereKette(wimpelliste);
-            if (ergebnisse.isEmpty() || neuesErgebnis.istBesser(ergebnisse.get(0))) {
-                ergebnisse.clear();
-                ergebnisse.add(neuesErgebnis);
-            } else if (neuesErgebnis.istGleich(ergebnisse.get(0))) {
-                ergebnisse.add(neuesErgebnis);
-            }
-            
-            return;
-        }
-
-        for (int i = index; i < wimpelliste.size(); i++) {
-            tausche(wimpelliste, index, i);
-            generierePermutationen(wimpelliste, index + 1, ergebnisse);
-            tausche(wimpelliste, index, i);
-        }
-    }
-
-    private void tausche(ArrayList<Wimpel> wimpelliste, int index1, int index2) {
-        Wimpel temp = wimpelliste.get(index1);
-        wimpelliste.set(index1, wimpelliste.get(index2));
-        wimpelliste.set(index2, temp);
-    }
-
-    private Ergebnis evaluiereKette(ArrayList<Wimpel> wimpelliste) {
-        int minDistanz = Integer.MAX_VALUE;
-        int minFrequenz = 0;
-
-        HashMap<Character, Integer> letztePosition = new HashMap<>();
-
-        for (int i = 0; i < wimpelliste.size(); i++) {
-            char farbe = wimpelliste.get(i).getFarbe();
-            if (letztePosition.containsKey(farbe)) {
-                int distanz = i - letztePosition.get(farbe);
-                if (distanz < minDistanz) {
-                    minDistanz = distanz;
-                    minFrequenz = 1;
-                } else if (distanz == minDistanz) {
-                    minFrequenz++;
+        public boolean besserAls(Qualitaet other) {
+            if (this.minimalAbstand > other.minimalAbstand) {
+                return true;
+            } else if (this.minimalAbstand < other.minimalAbstand) {
+                return false;
+            } else {
+                if (this.haeufigkeit < other.haeufigkeit) {
+                    return true;
+                } else {
+                    return false;
                 }
             }
-            letztePosition.put(farbe, i);
         }
+    }
 
-        StringBuilder ketteBuilder = new StringBuilder();
-        for (Wimpel wimpel : wimpelliste) {
-            ketteBuilder.append(wimpel.getFarbe());
+    public enum WimpelFarbe {
+        ROT, BLAU, WEISS, SCHWARZ, GELB;
+    }
+
+    public Wimpelkette2() {
+        this.wimpelListe = new ArrayList<>();
+    }
+
+    public Wimpelkette2(List<WimpelFarbe> wimpelListe) {
+        this.wimpelListe = new ArrayList<>(wimpelListe);
+    }
+
+    public Wimpelkette2(Wimpelkette2 wimpelkette) {
+        this(wimpelkette.wimpelListe);
+    }
+
+    public static Wimpelkette2 bestmoeglicheKette(HashMap<WimpelFarbe, Integer> restWimpel) {
+        return bestmoeglicheKette(new Wimpelkette2(), restWimpel);
+    }
+
+    public static Wimpelkette2 bestmoeglicheKette(Wimpelkette2 kette, HashMap<WimpelFarbe, Integer> restWimpel) {
+        if (restWimpel.isEmpty()) {
+            kette.berechneQualitaet();
+            return kette;
         }
-        String kette = ketteBuilder.toString();
+        List<Wimpelkette2> optionenAnKetten = new LinkedList<>();
+        for (WimpelFarbe farbe : restWimpel.keySet()) {
+            Wimpelkette2 neueKette = addFarbeToKette(kette, farbe);
+            HashMap<WimpelFarbe, Integer> neueRestWimpel = removeFarbeFromRest(restWimpel, farbe);
+            optionenAnKetten.add(bestmoeglicheKette(neueKette, neueRestWimpel));
+        }
+        // qualitaeten zaehlen und qualitaet anhaengen, bzw schwesterketten
+        optionenAnKetten.sort((wimpelkette, t1) -> {
+            if (wimpelkette.qualitaet.besserAls(t1.qualitaet)) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+        return optionenAnKetten.get(0);
+    }
 
-        return new Ergebnis(minDistanz, minFrequenz, kette);
+    private static Wimpelkette2 addFarbeToKette(Wimpelkette2 kette, WimpelFarbe farbe) {
+        Wimpelkette2 neueKette = new Wimpelkette2(kette);
+        neueKette.wimpelListe.add(farbe);
+        return neueKette;
+    }
+
+    private static HashMap<WimpelFarbe, Integer> removeFarbeFromRest(HashMap<WimpelFarbe, Integer> restWimpel,
+            WimpelFarbe farbe) {
+        HashMap<WimpelFarbe, Integer> neueRestWimpel = new HashMap(restWimpel);
+        if (neueRestWimpel.get(farbe) > 1) {
+            neueRestWimpel.put(farbe, neueRestWimpel.get(farbe) - 1);
+        } else {
+            neueRestWimpel.remove(farbe);
+        }
+        return neueRestWimpel;
+    }
+
+    private void berechneQualitaet() {
+        qualitaet = new Qualitaet();
+        qualitaet.minimalAbstand = Double.POSITIVE_INFINITY;
+        for (WimpelFarbe farbe : WimpelFarbe.values()) {
+            qualitaet.minimalAbstand = Double.min(qualitaet.minimalAbstand, getMinAbstand(farbe));
+        }
+        qualitaet.haeufigkeit = 0;
+        for (WimpelFarbe farbe : WimpelFarbe.values()) {
+            qualitaet.haeufigkeit += zaehleMinAbstand(farbe, qualitaet.minimalAbstand);
+        }
+    }
+
+    private double getMinAbstand(WimpelFarbe farbe) {
+        double minAbstand = Double.POSITIVE_INFINITY;
+        double lastPosition = Double.NEGATIVE_INFINITY;
+        double position = 0.0;
+        for (WimpelFarbe wimpel : wimpelListe) {
+            if (farbe == wimpel) {
+                double abstand = position - lastPosition - 1;
+                minAbstand = Double.min(minAbstand, abstand);
+                lastPosition = position;
+            }
+            position++;
+        }
+        return minAbstand;
+    }
+
+    private int zaehleMinAbstand(WimpelFarbe farbe, double minAbstand) {
+        double lastPosition = Double.NEGATIVE_INFINITY;
+        double position = 0.0;
+        int anzahl = 0;
+        for (WimpelFarbe wimpel : wimpelListe) {
+            if (farbe == wimpel) {
+                double abstand = position - lastPosition - 1;
+                if (minAbstand == abstand) {
+                    anzahl++;
+                }
+                ;
+                lastPosition = position;
+            }
+            position++;
+        }
+        return anzahl;
+    }
+
+    public void aufhaengen() {
+        System.out.print("---");
+        for (WimpelFarbe farbe : wimpelListe) {
+            System.out.print(farbe.name().charAt(0));
+        }
+        System.out.println("---");
+    }
+
+    public static void main(String[] args) {
+        // test1();
+        // test2();
+        // test3();
+        run3();
+        run2();
+        run1();
+    }
+
+    private static void run3() {
+        HashMap<WimpelFarbe, Integer> verfuegbareWimpel = new HashMap<>();
+        verfuegbareWimpel.put(WimpelFarbe.ROT, 3);
+        verfuegbareWimpel.put(WimpelFarbe.BLAU, 3);
+        verfuegbareWimpel.put(WimpelFarbe.WEISS, 3);
+        Wimpelkette2 besteKette = bestmoeglicheKette(verfuegbareWimpel);
+        besteKette.aufhaengen();
+        System.out.println(
+                "Qualität(" + besteKette.qualitaet.minimalAbstand + ", " + besteKette.qualitaet.haeufigkeit + ")");
+    }
+
+    private static void run2() {
+        HashMap<WimpelFarbe, Integer> verfuegbareWimpel = new HashMap<>();
+        verfuegbareWimpel.put(WimpelFarbe.ROT, 1);
+        verfuegbareWimpel.put(WimpelFarbe.BLAU, 2);
+        verfuegbareWimpel.put(WimpelFarbe.WEISS, 3);
+        verfuegbareWimpel.put(WimpelFarbe.SCHWARZ, 4);
+        Wimpelkette2 besteKette = bestmoeglicheKette(verfuegbareWimpel);
+        besteKette.aufhaengen();
+        System.out.println(
+                "Qualität(" + besteKette.qualitaet.minimalAbstand + ", " + besteKette.qualitaet.haeufigkeit + ")");
+    }
+
+    private static void run1() {
+        HashMap<WimpelFarbe, Integer> verfuegbareWimpel = new HashMap<>();
+        verfuegbareWimpel.put(WimpelFarbe.ROT, 4);
+        verfuegbareWimpel.put(WimpelFarbe.BLAU, 3);
+        verfuegbareWimpel.put(WimpelFarbe.WEISS, 4);
+        verfuegbareWimpel.put(WimpelFarbe.SCHWARZ, 2);
+        verfuegbareWimpel.put(WimpelFarbe.GELB, 2);
+        Wimpelkette2 besteKette = bestmoeglicheKette(verfuegbareWimpel);
+        besteKette.aufhaengen();
+        System.out.println(
+                "Qualität(" + besteKette.qualitaet.minimalAbstand + ", " + besteKette.qualitaet.haeufigkeit + ")");
+    }
+
+    private static void test1() {
+        List<WimpelFarbe> testWimpelListe = new ArrayList<>();
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        Wimpelkette2 testKette = new Wimpelkette2(testWimpelListe);
+        testKette.berechneQualitaet();
+        System.out.print("Qualität(0.0,3)==");
+        System.out.println(
+                "Qualität(" + testKette.qualitaet.minimalAbstand + ", " + testKette.qualitaet.haeufigkeit + ")");
+    }
+
+    private static void test2() {
+        List<WimpelFarbe> testWimpelListe = new ArrayList<>();
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        Wimpelkette2 testKette = new Wimpelkette2(testWimpelListe);
+        testKette.berechneQualitaet();
+        System.out.print("Qualität(0.0,5)==");
+        System.out.println(
+                "Qualität(" + testKette.qualitaet.minimalAbstand + ", " + testKette.qualitaet.haeufigkeit + ")");
+    }
+
+    private static void test3() {
+        List<WimpelFarbe> testWimpelListe = new ArrayList<>();
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.ROT);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.WEISS);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        testWimpelListe.add(WimpelFarbe.WEISS);
+        testWimpelListe.add(WimpelFarbe.BLAU);
+        Wimpelkette2 testKette = new Wimpelkette2(testWimpelListe);
+        testKette.berechneQualitaet();
+        System.out.print("Qualität(0.0,9)==");
+        System.out.println(
+                "Qualität(" + testKette.qualitaet.minimalAbstand + ", " + testKette.qualitaet.haeufigkeit + ")");
     }
 }
